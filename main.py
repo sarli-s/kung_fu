963cbc7b911e76856e58b22e826073f752f6912d
sarli sprei <sarli37531@gmail.com>
2026-07-12 16:37:58 +0300

    add run.py file and clean the main.py file

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-import sys
-from chess.services.input_parser import parse_input
-from chess.services.board_builder import build_board
-from chess.core.controller import handle_commands
-from chess.utils.errors import ChessError
-
-def main(parser=build_board, handler=handle_commands):
-    text = sys.stdin.read()
-    board_lines, command_lines = parse_input(text)
-    try:
-        board = parser(board_lines)
-    except ChessError as e:
-        print(e)
-        return
-    handler(board, command_lines)
+from run import main
 
 if __name__ == "__main__":
     main()
